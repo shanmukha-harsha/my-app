6a145af1751d4090516b80cbde917735b04a73cd
Shanmukha Sri Harsha Anivilla <sriharsha.shanmukha@gmail.com>
2023-07-06 19:10:51 +0530

    google added4

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -15,7 +15,7 @@ const ProjectDetails = () => {
                 <p>Application that scores essay inputs based on 6 parameters.</p>
                 <p>Used RoBERTa and DeBERTa, compared them.</p>
                 <p>Achieved a Mean Columnwise RMSE loss of 0.43</p>
-                <Link to={{ pathname: "https://www.google.com/" }} target="_blank" className="btn">LEARN MORE</Link>
+                <a href="https://www.google.com/" target="_blank" className="btn">LEARN MORE</a>
             </div>
 
             <div className="card">
